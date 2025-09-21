7e6c780da6c001b65205d49ad04a4429a49e863e
careb <carolina.pereira@utec.edu.uy>
2025-09-21 17:14:18 -0300

    Add redirect from /messages/ to /messages/inbox/

--- ecommerce_project/messages/urls.py
+++ ecommerce_project/messages/urls.py
@@ -3,10 +3,12 @@ This module defines URL patterns for the messages app.
 """
 
 from django.urls import path
+from django.shortcuts import redirect
 from .views import MessageInboxView, MessageSentView, MessageDetailView, MessageCreateView, MessageDeleteView
 
 # URL patterns for the messages app
 urlpatterns = [
+    path('', lambda request: redirect('message_inbox'), name='message_index'),
     path('inbox/', MessageInboxView.as_view(), name='message_inbox'),
     path('sent/', MessageSentView.as_view(), name='message_sent'),
     path('<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
